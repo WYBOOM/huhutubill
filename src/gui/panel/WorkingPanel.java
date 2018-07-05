47b079b64799a48341890e092e91f8b94dc65f81
WYBOOM <38638689+WYBOOM@users.noreply.github.com>
2018-07-05 14:53:48 +0800

    Revert "Initial commit"
    This reverts commit 4a47092e7091c63b5608b3764c9554c2f735dda8.

--- src/gui/panel/WorkingPanel.java
+++
@@ -1,9 +0,0 @@
-package gui.panel;
-
-import javax.swing.JPanel;
-
-public abstract class  WorkingPanel extends JPanel  {
-	public abstract  void addListener();
-	public abstract  void updateData();
-	
-}
